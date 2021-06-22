fb587d9b267498bdddaabbabac6eb1f472eb2d46
Delicious Tofu <CodingAhn@gmail.com>
2021-06-22 12:17:23 +0900

    change name

---
+++ var_cons.go
@@ -0,0 +1,23 @@
+package main
+
+func var_cons() {
+	var a int = 2
+	a += 2
+	b := 3
+	b += 1
+
+	const c int = 10
+	const s string = "hi"
+	const x = 20
+	const y = "hello"
+	const (
+		i = "abc"
+		j = "xyz"
+	)
+	const (
+		i0 = iota
+		i1
+		i2
+	)
+	println(i0, i1, i2)
+}
